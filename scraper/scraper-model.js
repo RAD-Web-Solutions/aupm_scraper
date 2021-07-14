315670cc58af6b5d44247f025a6411b89ec59040
Chad Simpson <mrsimpson3000@gmail.com>
2021-07-14 14:51:12 -0400

    Added Ask price for all the metals.

--- scraper/scraper-model.js
+++ scraper/scraper-model.js
@@ -14,34 +14,44 @@ async function scraper() {
         const AUdate = (dom.window.document.getElementById('AU-date').textContent)
         const AUtime = (dom.window.document.getElementById('AU-time').textContent)
         const AUbid = (dom.window.document.getElementById('AU-bid').textContent)
+        const AUask = (dom.window.document.getElementById('AU-ask').textContent)
         const AGdate = (dom.window.document.getElementById('AG-date').textContent)
         const AGtime = (dom.window.document.getElementById('AG-time').textContent)
         const AGbid = (dom.window.document.getElementById('AG-bid').textContent)
+        const AGask = (dom.window.document.getElementById('AG-ask').textContent)
         const PTdate = (dom.window.document.getElementById('PT-date').textContent)
         const PTtime = (dom.window.document.getElementById('PT-time').textContent)
         const PTbid = (dom.window.document.getElementById('PT-bid').textContent)
+        const PTask = (dom.window.document.getElementById('PT-ask').textContent)
         const PDdate = (dom.window.document.getElementById('PD-date').textContent)
         const PDtime = (dom.window.document.getElementById('PD-time').textContent)
         const PDbid = (dom.window.document.getElementById('PD-bid').textContent)
+        const PDask = (dom.window.document.getElementById('PD-ask').textContent)
         const RHdate = (dom.window.document.getElementById('RH-date').textContent)
         const RHtime = (dom.window.document.getElementById('RH-time').textContent)
         const RHbid = (dom.window.document.getElementById('RH-bid').textContent)
+        const RHask = (dom.window.document.getElementById('RH-ask').textContent)
         const result = {
             'AUdate':AUdate,
             'AUtime':AUtime,
             'AUbid':AUbid,
+            'AUask':AUask,
             'AGdate':AGdate,
             'AGtime':AGtime,
             'AGbid':AGbid,
+            'AGask':AGask,
             'PTdate':PTdate,
             'PTtime':PTtime,
             'PTbid':PTbid,
+            'PTask':PTask,
             'PDdate':PDdate,
             'PDtime':PDtime,
             'PDbid':PDbid,
+            'PDask':PDask,
             'RHdate':RHdate,
             'RHtime':RHtime,
-            'RHbid':RHbid
+            'RHbid':RHbid,
+            'RHask':RHask
         }
         return (result)
 
